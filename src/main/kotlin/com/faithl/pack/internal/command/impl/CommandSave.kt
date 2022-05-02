0f28626081cf45bf7281a2642aa7df80bdd73e16
莱奥斯 <621862169@qq.com>
2022-05-02 18:44:58 +0800

    update save command

--- src/main/kotlin/com/faithl/pack/internal/command/impl/CommandSave.kt
+++ src/main/kotlin/com/faithl/pack/internal/command/impl/CommandSave.kt
@@ -3,16 +3,18 @@ package com.faithl.pack.internal.command.impl
 import com.faithl.pack.internal.database.Database
 import taboolib.common.platform.ProxyCommandSender
 import taboolib.common.platform.command.subCommand
+import taboolib.module.lang.sendLang
 
 object CommandSave {
 
     val command = subCommand {
         execute<ProxyCommandSender> { sender, _, _ ->
-            Database.cache.forEach { key, value ->
+            Database.cache.forEach { (key, value) ->
                 value.data.forEach {
                     Database.INSTANCE.setPackData(key, it)
                 }
             }
+            sender.sendLang("command-save-info")
         }
     }
 
